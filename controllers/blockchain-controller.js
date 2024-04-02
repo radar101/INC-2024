1ca3f15b1d9d29bafae444fbdff99a2412582dfe
Soaham <113767354+soaham-pimparkar@users.noreply.github.com>
2024-04-02 12:56:29 +0530

    server-blockchain comm functions added

--- controllers/blockchain-controller.js
+++ controllers/blockchain-controller.js
@@ -7,19 +7,46 @@ class Blockchain
     
     provider = `htps://mainnet.infura.io/v3/${INFURA_KEY}`; 
     web3 =  new Web3(this.provider);
+
     contract_abi = ABI_ID;
     contract_address = CONTRACT_ADDRESS;
+
     contract = new this.web3.eth.Contract(this.contract_abi,this.contract_address);
     account = this.web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
+
     constructor()
     {
 
     }
 
-    async addIpRecordTocontract()
+    async addIpRecordTocontract(newTitle, newIpType, newDescription, stnewProofs, stnewLinks, newExtrainfo, newLicenseType, newOwnerName, newOwnerProofType, newOwnerProofIdentifier, newOwnerDigitalSign)
     {
-            
-    }  
+        const newIpRecord = {newTitle, newIpType, newDescription, stnewProofs, stnewLinks, newExtrainfo, newLicenseType, newOwnerName, newOwnerProofType, newOwnerProofIdentifier, newOwnerDigitalSign};
+        //var documentId;
+
+        try{
+            const documentId = await contract.methods.addIpRecord(newIpRecord).send({from: account});
+            //handle storage of the document id in user's profile data
+            console.log('IP record added successfully! Id: ', documentId);
+            return documentId;
+        }catch(error){
+            console.log('Task exited with error: ', error);
+            return false;
+        }
+    }
+    
+    async readIpRecordToContract(newIpId)
+    {
+        try {
+            const ipRecord = await contract.methods.readIpRecord(newIpId)
+                                              .call();
+            console.log('IP Record:', ipRecord);
+            return ipRecord;
+        } catch (error) {
+            console.error('Error reading IP record:', error);
+            return false;
+        }
+    }
 
 
     
